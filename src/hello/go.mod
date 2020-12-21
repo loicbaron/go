2074131f151ad6bd73e5525fb5c5a06bd1e707f2
Loic Baron <loic.baron@gmail.com>
2020-12-21 10:03:36 +0000

    feat(module): add module example

---
+++ src/hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/loicbaron/go/hello
+
+go 1.15
---
+++ src/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/loicbaron/go/hello/morestrings"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
+h
