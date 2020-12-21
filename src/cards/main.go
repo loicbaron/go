4d30bdb757a23bc1c1027be5b36ccaec61142755
Loic Baron <loic.baron@gmail.com>
2020-12-21 14:00:13 +0000

    feat(cards): add deck type

--- src/cards/main.go
+++ src/cards/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of Dimonds", newCard()}
+	cards := deck{"Ace of Dimonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
