d1465532e108267dd75e37e2571650a6ed5be8f4
Loic Baron <loic.baron@gmail.com>
2020-12-21 16:25:48 +0000

    feat(cards): add deal func

--- src/cards/main.go
+++ src/cards/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards = append(cards, "Six of Spades")
-
+	hand, remaining := deal(cards, 5)
+	fmt.Println("hand = ")
+	hand.print()
+	fmt.Println("remaining = ")
+	remaining.print()
+	fmt.Println("cards = ")
 	cards.print()
 }
