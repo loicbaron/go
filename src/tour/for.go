55fb071b78b379eca88168e98008edbefbcf15f2
Loic Baron <loic.baron@gmail.com>
2020-12-21 10:00:54 +0000

    feat(for): add for loop example

---
+++ src/tour/for.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func forExample() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func whileExample() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	forExample()
+	whileExample()
+}
