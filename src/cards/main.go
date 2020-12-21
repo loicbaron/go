5e7726bc85e7111101d43cc954e951a4b102bc87
Loic Baron <loic.baron@gmail.com>
2020-12-21 16:34:59 +0000

    feat(cards): add toString

--- src/cards/main.go
+++ src/cards/main.go
@@ -5,11 +5,7 @@ import "fmt"
 func main() {
 	cards := newDeck()
 	cards = append(cards, "Six of Spades")
-	hand, remaining := deal(cards, 5)
-	fmt.Println("hand = ")
-	hand.print()
-	fmt.Println("remaining = ")
-	remaining.print()
-	fmt.Println("cards = ")
-	cards.print()
+	// hand, remaining := deal(cards, 5)
+
+	fmt.Println(cards.toString())
 }
